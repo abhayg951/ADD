6a32b4f76d235125e4cf4b4af0926141c2183ddd
Abhay Gupta <109977125+abhayg951@users.noreply.github.com>
2023-08-23 01:32:49 +0530

    Update settings.py

--- ADD/settings.py
+++ ADD/settings.py
@@ -104,13 +104,12 @@ WSGI_APPLICATION = 'ADD.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'URL': os.getenv('POSTGRES_URL'),
         'NAME': os.getenv('POSTGRES_DATABASE'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
         'HOST': os.getenv('POSTGRES_HOST'),
-        # 'PORT': 6129,
     }
 }
 
