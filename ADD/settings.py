9d64dccb8e911955015cdf058f4ec685c283667c
Abhay Gupta <109977125+abhayg951@users.noreply.github.com>
2023-08-23 00:49:50 +0530

    vercel db added

--- ADD/settings.py
+++ ADD/settings.py
@@ -78,12 +78,12 @@ WSGI_APPLICATION = 'ADD.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 import os
 
@@ -99,17 +99,17 @@ import os
 #     }
 # }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'URL': "postgres://default:e9BIPzsjE6Oq@ep-quiet-shadow-16202443-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb",
-#         'NAME': "verceldb",
-#         'USER': "default",
-#         'PASSWORD': "e9BIPzsjE6Oq",
-#         'HOST': "ep-quiet-shadow-16202443-pooler.us-east-1.postgres.vercel-storage.com",
-#         # 'PORT': 6129,
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'URL': "postgres://default:e9BIPzsjE6Oq@ep-quiet-shadow-16202443-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb",
+        'NAME': "verceldb",
+        'USER': "default",
+        'PASSWORD': "e9BIPzsjE6Oq",
+        'HOST': "ep-quiet-shadow-16202443-pooler.us-east-1.postgres.vercel-storage.com",
+        # 'PORT': 6129,
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
