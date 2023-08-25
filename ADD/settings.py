f67d82445b1b7fd49fb04ebaad5c245fcf68ce13
Abhay Gupta <109977125+abhayg951@users.noreply.github.com>
2023-08-26 01:10:15 +0530

    settings changed

--- ADD/settings.py
+++ ADD/settings.py
@@ -149,9 +149,9 @@ USE_TZ = True
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 465
-EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.getenv('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASS')
 EMAIL_USE_TLS = True
 
 
