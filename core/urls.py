42baffd72a9d2fa0e420e49d00f4eda28dd51674
Abhay Gupta <109977125+abhayg951@users.noreply.github.com>
2023-09-10 18:25:36 +0530

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -7,4 +7,4 @@ urlpatterns = [
     path("product", ProductAPIView.as_view())
 ]
 
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
